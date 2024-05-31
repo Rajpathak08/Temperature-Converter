d4a6f5e3462e4aefca3277bab0a3c36655b19c99
Rajpathak08 <96427679+Rajpathak08@users.noreply.github.com>
2024-06-01 00:41:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,79 @@
-const celciusEl = document.getElementById("Celcius");
-const fahrenheitEl = document.getElementById("Fahrenheit");
-const kelvinEl = document.getElementById("Kelvin");
+      function convert(){
+            var fromUnit = document.getElementById('fromUnit').value;
+            var toUnit = document.getElementById('toUnit').value;
+            var temperature = parseFloat(document.getElementById('temperature').value);
 
-function computeTemp(event) {
-    const currentValue = event.target.value;
+            var converttedValue;
+            var resultUnit;
 
-    switch (event.target.name) {
-        case "Celcius":
-            fahrenheitEl.value = (currentValue * 9 / 5) + 32;
-            kelvinEl.value = parseFloat(currentValue) + 273.15;
-            break;
+            // Perform conversion logic
 
-        case "Fahrenheit":
-            celciusEl.value = (currentValue - 32) * 5 / 9;
-            kelvinEl.value = (currentValue - 32) * 5 / 9 + 273.15;
-            break;
+            switch (fromUnit){
+                case 'C':
+                    if (toUnit === 'F'){
+                        convertedValue = (temperature * 9/5) +32;
+                        resultUnit = 'F';
+                    } else if (toUnit === 'K'){
+                        convertedValue = temperature + 273.15;
+                        resultUnit = 'K';
+                    }else if (toUnit === 'R'){
+                        convertedValue = temperature + 273.15;
+                        resultUnit = 'R';
+                    }else{
+                        convertedValue = temperature;
+                        resultUnit = 'C';
+                    }
+                    break;
 
-        case "Kelvin":
-            celciusEl.value = currentValue - 273.15;
-            fahrenheitEl.value = (currentValue - 273.15) * 9 / 5 + 32;
-            break;
+                    case 'F':
+                    if (toUnit === 'C'){
+                        convertedValue = (temperature -32) * 5/9;
+                        resultUnit = 'C';
+                    } else if (toUnit === 'K'){
+                        convertedValue = (temperature -32) * 5/9 + 273.15;
+                        resultUnit = 'K';
+                    }else if (toUnit === 'R'){
+                        convertedValue = temperature + 459.67;
+                        resultUnit = 'R';
+                    }else{
+                        convertedValue = temperature;
+                        resultUnit = 'F';
+                    }
+                    break;
 
-        default:
-            break;
-    }
-}
+                    case 'K':
+                    if (toUnit === 'C'){
+                        convertedValue = temperature - 273.15;
+                        resultUnit = 'C';
+                    } else if (toUnit === 'K'){
+                        convertedValue = (temperature - 273.15) * 9/5 +32;
+                        resultUnit = 'K';
+                    }else if (toUnit === 'R'){
+                        convertedValue = temperature * 9/5;
+                        resultUnit = 'R';
+                    }else{
+                        convertedValue = temperature;
+                        resultUnit = 'K';
+                    }
+                    break;
+
+                    case 'R':
+                    if (toUnit === 'C'){
+                        convertedValue = (temperature - 491.67) * 5/9;
+                        resultUnit = 'C';
+                    } else if (toUnit === 'K'){
+                        convertedValue = temperature - 459.67;
+                        resultUnit = 'K';
+                    }else if (toUnit === 'R'){
+                        convertedValue = temperature * 5/9;
+                        resultUnit = 'R';
+                    }else{
+                        convertedValue = temperature;
+                        resultUnit = 'R';
+                    }
+                    break;
+            }
+
+            document.getElementById('result').value = convertedValue.toFixed(2) + ' ' + resultUnit;
+            
+        }
